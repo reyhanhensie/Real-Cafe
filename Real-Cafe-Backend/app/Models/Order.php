5135168d9b859225533bfccb02077c14b04e6838
reyhanhensie <reyhanhensie@gmail.com>
2024-08-26 22:10:19 +0700

    add message

--- Real-Cafe-Backend/app/Models/Order.php
+++ Real-Cafe-Backend/app/Models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['total_price','meja_no', 'status']; // Include 'status' in fillable
+    protected $fillable = ['total_price','meja_no','message', 'status']; // Include 'status' in fillable
 
     // Define the relationship with OrderItem
     public function items()
