3641a98d3201caef75da7ae46b074c140ff08cfe
reyhanhensie <reyhanhensie@gmail.com>
2024-09-01 06:10:34 +0700

    deploy order-form

--- Real-Cafe-Backend/resources/views/index.blade.php
+++ Real-Cafe-Backend/resources/views/index.blade.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>Real Cafe</title>
-        <link rel="stylesheet" href="/index.css" />
-        <script defer="defer" src="/static/js/main.fa631a65.js"></script>
-        <link href="/static/css/main.b153a4a0.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div id="root"></div>
-    </body>
-</html>
-
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Real Cafe</title><link rel="stylesheet" href="/index.css"><script defer="defer" src="/static/js/main.067785dd.js"></script><link href="/static/css/main.4e6eda1a.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
