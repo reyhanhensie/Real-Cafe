62a9cfe242d99a17a081638962f6068516fa547b
reyhanhensie <reyhanhensie@gmail.com>
2024-08-31 15:43:20 +0700

    deploy frontend to backend

---
+++ Real-Cafe-Backend/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Real Cafe</title>
+        <link rel="stylesheet" href="/index.css" />
+        <script defer="defer" src="/static/js/main.1788429f.js"></script>
+        <link href="/static/css/main.b153a4a0.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+</html>
