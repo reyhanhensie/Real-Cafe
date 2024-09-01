ece718dfb61176730a76c90889395a6cc8cdab3b
reyhanhensie <reyhanhensie@gmail.com>
2024-09-02 03:16:51 +0700

    navbar change

--- Real-Cafe-Frontend/src/components/Navbar.js
+++ Real-Cafe-Frontend/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-// import logo from '../assets/logo.png';
+import logo from '../assets/images/logo.png';
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-      {/* <img src={logo} alt="Logo" /> */}
+      <img src={logo} alt="Logo" />
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
