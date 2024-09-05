d57ab48e7e1bd34f8cb5d23538795b15d1724008
reyhanhensie <reyhanhensie@gmail.com>
2024-09-06 04:33:33 +0700

    navigation update

--- Real-Cafe-Frontend/src/components/Navbar.js
+++ Real-Cafe-Frontend/src/components/Navbar.js
@@ -10,8 +10,9 @@ const Navbar = () => {
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/order-form">Cashier</Link></li>
+        <li><Link to="/order-summary">Kitchen Order</Link></li>
+        <li><Link to="/login">Login</Link></li>
       </ul>
     </nav>
   );
