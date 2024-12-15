fac4418eea9116979ceeab404edc5ec70522e974
reyhanhensie <reyhanhensie@gmail.com>
2024-12-16 00:12:43 +0700

    deploy frontend

--- Real-Cafe-Backend/resources/views/index.blade.php
+++ Real-Cafe-Backend/resources/views/index.blade.php
@@ -1,18 +1,14 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Real Cafe</title>
-    <link rel="stylesheet" href="/index.css">
-    <script defer="defer" src="/static/js/main.067785dd.js"></script>
-    <link href="/static/css/main.4e6eda1a.css" rel="stylesheet">
-</head>
-
-<body>
-    <div id="root"></div>
-</body>
-
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Real Cafe</title>
+        <link rel="stylesheet" href="/index.css" />
+        <script defer="defer" src="/static/js/main.23460141.js"></script>
+        <link href="/static/css/main.67385be0.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
 </html>
--- Real-Cafe-Frontend/src/apiconfig.js
+++ Real-Cafe-Frontend/src/apiconfig.js
@@ -4,7 +4,7 @@ let API_URL;
 if (process.env.NODE_ENV === 'development') {
     API_URL = 'http://127.0.0.1:8000/api'; // Local development API
 } else {
-    API_URL = 'http://192.168.1.3/api'; // Production API (relative path)
+    API_URL = 'http://localhost:8000/api'; // Production API (relative path)
 }
 
 export default API_URL;
