21e853418181a8b359cfc8b4c92856c9d1d51e07
reyhanhensie <reyhanhensie@gmail.com>
2024-12-17 23:43:40 +0700

    change api url

--- Real-Cafe-Backend/resources/views/index.blade.php
+++ Real-Cafe-Backend/resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Real Cafe</title>
         <link rel="stylesheet" href="/index.css" />
-        <script defer="defer" src="/static/js/main.23460141.js"></script>
+        <script defer="defer" src="/static/js/main.085e72e1.js"></script>
         <link href="/static/css/main.67385be0.css" rel="stylesheet" />
     </head>
     <body>
