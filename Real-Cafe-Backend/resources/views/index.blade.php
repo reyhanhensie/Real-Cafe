6032267208b8b004f2df6f3675ad32a0db00a612
reyhanhensie <reyhanhensie@gmail.com>
2024-12-18 23:06:02 +0700

    deploy fe

--- Real-Cafe-Backend/resources/views/index.blade.php
+++ Real-Cafe-Backend/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Real Cafe</title>
     <link rel="stylesheet" href="/index.css">
-    <script defer="defer" src="/static/js/main.88994cac.js"></script>
+    <script defer="defer" src="/static/js/main.b3856449.js"></script>
     <link href="/static/css/main.67385be0.css" rel="stylesheet">
 </head>
 
