d2d319fd2ac3012801cad4f840bedbb93dffa1b7
reyhanhensie <reyhanhensie@gmail.com>
2025-01-29 00:01:10 +0700

    forgot to add type "paket"

--- Real-Cafe-Backend/app/Http/Controllers/OrderController.php
+++ Real-Cafe-Backend/app/Http/Controllers/OrderController.php
@@ -96,7 +96,7 @@ class OrderController extends Controller
             'meja' => 'required|integer', // Validate meja_no\
             'kasir' => 'required',
             'items' => 'required|array',
-            'items.*.type' => 'required|string|in:camilan,coffe,jus,lalapan,milkshake,makanan,minumandingin,minumanpanas',
+            'items.*.type' => 'required|string|in:camilan,coffe,jus,lalapan,milkshake,makanan,minumandingin,minumanpanas,paket',
             'items.*.id' => 'required|integer',
             'items.*.qty' => 'required|integer|min:1',
             'bayar' => 'required|integer|:0', // Payment amount
